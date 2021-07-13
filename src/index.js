98a60704ef46788c0fec662c1067c646ab273a5d
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-07-13 00:01:57 -0500

    Función de recolección de link ya tiene acceso a la ruta, función de validación completa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs'); 
 const fsPromises = require('fs').promises;
 const markdownLinkExtractor = require('markdown-link-extractor');
+const axios = require('axios')
 
 /* Función de recolección de rutas con formato .md */
 let filesArray = []
@@ -21,32 +22,92 @@ const getFiles = (route) =>{
             }
         }
     }
-    if(fs.existsSync(absolutRoute) && path.extname(absolutRoute) === ".md"){
+    if(fs.existsSync(absolutRoute) && path.extname(absolutRoute) === ".md"  ){
         filesArray.push(absolutRoute)
     }
     return filesArray
 }
 
+
 /* Función de lectura de rutas .md recolectadas*/
-const readFiles = (files) => {
+const readFiles = async (files) => {
     const textPromise = files.map(function(path){
-        return (fs.promises.readFile(path,'utf8'))
+        return fs.promises.readFile(path,'utf8')
     });
     return Promise.all(textPromise).then((text) =>{
-        const a = text.join();
-        return a        
+        const propertiesArray = text.map((item,index)=>{
+            const object = {
+                text: item,
+                path: files[index]
+            }
+            return object  
+        })
+        return propertiesArray
+    })   
+}
+
+/*Función de recolección de los links en los archivos*/
+const getLinks = async (files) => {
+    return readFiles(files).then((content) =>{
+        const fillFiles = content.filter(item => {
+            return item.text !== ''
+        })
+        const links = fillFiles.map(item => {
+            const linkDetails = markdownLinkExtractor(item.text, true);
+            const linksArray = []
+            linkDetails.forEach( link => {
+                if (link.href.startsWith('http')){
+                    const objectLink = {
+                        href: link.href,
+                        text: link.text,
+                        file: item.path
+                    }
+                    linksArray.push(objectLink)
+                }
+            });
+            return linksArray    
+        })
+        return links
     })
 }
 
-
-/*Función de recolección de los links en los archivos*/
-const getLinks = (files) => {
-    readFiles(files).then((content) =>{
-        const links = markdownLinkExtractor(content, true);
-        links.forEach(link => console.log(link))
+/*Función de validación de los links recolectados*/
+const validateLinks = async (files) =>{
+    return getLinks(files).then(array =>{
+        return array.map( item => {
+            item.forEach(object =>{
+                const validateArray = []
+                return axios.get(object.href)
+                .then(response =>{
+                    const successObject = {
+                        href: object.href,
+                        text: object.text,
+                        file: object.file,
+                        status: response.status,
+                        statusText: 'ok'
+                    }
+                    validateArray.push(successObject) 
+                    return validateArray  
+                })
+                .catch(error =>{
+                    const failObject = {
+                        href: object.href,
+                        text: object.text,
+                        file: object.file,
+                        status: response.status,
+                        statusText: 'fail'
+                    }
+                    validateArray.push(failObject) 
+                    return validateArray
+                })
+           })         
+        })
     })
-} 
+}
 
-//console.log(getLinks(readFiles(getFiles('src/example-directory'))))
-//console.log(getFiles('src/example-directory'))
-getLinks(getFiles('README.md'))
+validateLinks(getFiles('src/example-directory'))
+
+module.exports = {
+    getFiles,
+    validateLinks
+}
